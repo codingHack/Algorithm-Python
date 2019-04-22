8ae9989d29e956451192b942af21a20f2f381df0
codingHack <dbstjdwo1000@naver.com>
2019-04-22 18:05:09 +0900

    Create P1009.py

---
+++ BackJoon/P1009.py
@@ -0,0 +1,18 @@
+N=int(input())
+for _ in range(N):
+    stack=[]
+    arr=list(map(lambda x:int(x),input().split()))
+    cur=arr[0]%10
+    for i in range(0,arr[1]):
+        if len(stack)==0:
+            stack.append(cur)
+        else:
+            nam=cur*arr[0]%10
+            if stack[0]==nam:
+                break
+            else:
+                stack.append(nam)
+                cur=nam
+    print(stack)
+    print(stack[arr[1]%len(stack)-1])
+    
