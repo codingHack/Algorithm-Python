a10312e6f3a6c8c90047d83aca357e5b2eda62e3
codingHack <dbstjdwo1000@naver.com>
2019-04-23 17:45:46 +0900

    Update P1516.py

--- BackJoon/P1516.py
+++ BackJoon/P1516.py
@@ -1,17 +1,46 @@
-def countTime(num):
-    if len(arr[num])==2:
-        return arr[num][0]
-    else:
-        max=0
-        for i in range(1,len(arr[num])-1):
-            a=countTime(arr[num][i])
-            if a>max:
-                max=a
-        return max+arr[num][0]
+N=int(input())
+graph=[[] for i in range(N+1)]
+cost=[0 for i in range(N+1)]
+indegree=[ 0 for i in range(N+1)]
+result=[]
 
-building_num=int(input())
-arr=[[0,0]]
-[arr.append(list(map(lambda x:int(x),input().split()))) for _ in range(0,building_num)]
+for i in range(1,N+1):
+    arr=list(map(lambda x:int(x),input().split()))
+    cost[i]=arr[0]
+    for j in range(1,len(arr)-1):
+        graph[i].append(arr[j])
+        indegree[arr[j]]+=1
 
-for i in range(1,building_num+1):
-    print(countTime(i))
+#print(graph)
+#print(cost)
+#print(indegree)
+
+def topologicalSort():
+    queue=[]
+    for i in range(1,N+1):
+        if indegree[i]==0:
+            queue.append(i)
+
+    while queue:
+        v=queue.pop(0)
+        result.append(v)
+        for i in graph[v]:
+            indegree[i]-=1
+            if indegree[i]==0:
+                queue.append(i)
+
+topologicalSort()
+result.reverse()
+#print(result)
+
+
+for i in range(1,len(result)):
+    maxNum=0
+    for j in graph[result[i]]:
+        if cost[j]>maxNum:
+            maxNum=cost[j]
+    cost[result[i]]=cost[result[i]]+maxNum 
+
+for i in range(1,len(cost)):
+    print(cost[i])
+        
